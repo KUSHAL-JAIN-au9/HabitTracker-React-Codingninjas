4b641256d1aa21c9c0ca8b17421d1b39e36f7037
Kushal Jain <jkushal069@gmail.com>
2023-06-24 18:45:26 +0530

    added hashrouter and removed browser router

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/HabitTracker-React-Codingninjas/",
+  base: "/HabitTracker-React-Codingninjas/",
   plugins: [react()],
 });
